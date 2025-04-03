7981c1017416e426316caf2d1992435739165946
vinod-makkala77 <your-email@example.com>
2025-04-03 14:58:21 +0530

    updated react

---
+++ cal(module).js
@@ -0,0 +1,23 @@
+export default function add(a, b) {
+  return a + b;
+}
+
+
+ function sub(a, b) {
+  return a - b;
+}
+function mul(a, b){
+    return a*b;
+}
+function div(a, b){
+    return a/b;
+}
+function str(){
+  return "this is fine u know"
+}
+function mybut(){
+  return (<button>
+    my button
+  </button>)
+}
+export{sub,mul,div,str,mybut};
---
+++ main.js
@@ -0,0 +1,10 @@
+
+import add,{sub,mul,div, str,mybut} from "./cal(module).js"
+let res=add(10,20);
+console.log(res); //30
+console.log(sub(10,20)); //30
+console.log(mul(10,20)); //200
+console.log(div(10,20)); //0.5
+console.log(str())
+console.log(mybut())
+console.log("main.js is working fine");
